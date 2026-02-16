7b225ad9a9d8e1ff1c60160fffc0faca06ab6667
hosteladventures2-oss <hosteladventures2@gmail.com>
2026-02-16 06:24:52 +0300

    Add Node.js Daraja API source code

---
+++ Functions/STK_push.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const http = require("http");
+const cors = require("cors");
+const router = express.Router();
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+
+
+const process = (getAccessTokens, app, axios, moment) =>{
+app.get("/stkpush", (req, res) => {
+  //getAccessToken()
+  getAccessTokens
+    .then((accessToken) => {
+      const url =
+        "https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+      const auth = "Bearer " + accessToken;
+      const timestamp = moment().format("YYYYMMDDHHmmss");
+      const password = new Buffer.from(
+        "4168059" +
+          "50630e57477fc855e55e6ce684fd4095606e4a93b52f7b6be5d270f3bac886d2" +
+          timestamp
+      ).toString("base64");
+
+      axios
+        .post(
+          url,
+          {
+            BusinessShortCode: "4168059",
+            Password: password,
+            Timestamp: timestamp,
+            TransactionType: "CustomerPayBillOnline",
+            Amount: "1",
+            PartyA: "254726270922", //phone number to receive the stk push
+            PartyB: "4168059",
+            PhoneNumber: "254726270922",
+            CallBackURL: "https://hosteladventures.site/Multi-fund/Aviator/mpesa_live/call_back_url.php",
+            AccountReference: "Rocketie",
+            TransactionDesc: "Mpesa Daraja API stk push test",
+          },
+          {
+            headers: {
+              Authorization: auth,
+            },
+          }
+        )
+        .then((response) => {
+          res.send("😀 Request is successful done ✔✔. Please enter mpesa pin to complete the transaction");
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send("❌ Request failed");
+        });
+    })
+    .catch(console.log);
+});
+}
+
+module.exports = process;
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const http = require("http");
+const cors = require("cors");
+const router = express.Router();
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+const stkpush = require('./Functions/STK_push')
+
+const port = process.env.PORT||3001;
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+//https://github.com/alvin-kiveu/Mpesa-Daraja-Api-NODE.JS/blob/master/app.js
+
+async function getAccessToken() {
+  const consumer_key = "zMYnc51rNFuT2AG3A26MTiGgoSPf19JWXEGd8u9EI9x06QGv"; // REPLACE IT WITH YOUR CONSUMER KEY
+  const consumer_secret = "M1WNjLGKyGxBrkWCh2Mye9mrRCkknnMwn1CJ1AGatxFvB9IBgLjax3KNm5DJr4Sf"; // REPLACE IT WITH YOUR CONSUMER SECRET
+  const url =
+    "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  const auth =
+    "Basic " +
+    new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: auth,
+      },
+    });
+   
+    const dataresponse = response.data;
+    // console.log(data);
+    const accessToken = dataresponse.access_token;
+    //console.log(accessToken)
+    return accessToken;
+  } catch (error) {
+    throw error;
+  }
+}
+
+//stkpush()
+//getAccessToken()
+const getAccessTokens = getAccessToken()
+
+app.get("/access_token", (req, res) => {
+  getAccessToken()
+    .then((accessToken) => {
+      res.send("😀 Your access token is " + accessToken);
+    })
+    .catch(console.log);
+});
+
+app.post("/callback", (req, res) => {
+  console.log("STK PUSH CALLBACK");
+  const CheckoutRequestID = req.body.Body.stkCallback.CheckoutRequestID;
+  const ResultCode = req.body.Body.stkCallback.ResultCode;
+  var json = JSON.stringify(req.body);
+  fs.writeFile("stkcallback.json", json, "utf8", function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("STK PUSH CALLBACK JSON FILE SAVED");
+  });
+  console.log(req.body);
+});
+
+
+
+stkpush(getAccessTokens, app, axios, moment)
+
+
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
