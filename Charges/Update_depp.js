e41a45405b94c58aba3f820f140b77ac25e962bf
kevincruize-create <kevincruize@gmail.com>
2026-03-08 15:47:47 +0300

    Update Update_depp.js

--- Charges/Update_depp.js
+++ Charges/Update_depp.js
@@ -28,7 +28,7 @@ const myID = 38
     }
   };
 
-  app.get("/Update_dep", async (req, res) => {
+  app.post("/Update_dep", async (req, res) => {
 
     const data = await fetchData();   // call your function
 
@@ -38,5 +38,6 @@ const myID = 38
 
 
 }
+
 
 module.exports = process;
