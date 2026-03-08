21053572f06026052e3777e1ff0a40e48b3bf2f4
kevincruize-create <kevincruize@gmail.com>
2026-03-08 15:46:42 +0300

    Update Deposited_update.js

--- Charges/Deposited_update.js
+++ Charges/Deposited_update.js
@@ -27,7 +27,7 @@ const myID = 38
     }
   };
 
-  app.get("/Deposited_update", async (req, res) => {
+  app.post("/Deposited_update", async (req, res) => {
 
     const data = await fetchData();   // call your function
 
@@ -37,5 +37,6 @@ const myID = 38
 
 
 }
+
 
 module.exports = process;
