0ff6ae9985478e4a72f3e137ee0285839940f55b
kevincruize-create <kevincruize@gmail.com>
2026-03-08 16:11:04 +0300

    Update Withdraw.js

--- Charges/Withdraw.js
+++ Charges/Withdraw.js
@@ -2,13 +2,13 @@ const express = require('express');
 //const app = express();
 
 const process = (app) => {
+app.use(express.json());
+//const myID = 38
+//const number = 254726270922
+//const amount = 200
 
-const myID = 38
-const number = 254726270922
-const amount = 200
 
-
-  const fetchData = async () => {
+  const fetchData = async (ID, amount_kes, mpesa_num) => {
     try {
 
       const response = await fetch("http://forexapi.atwebpages.com/Charges/Withdraw.php", {
@@ -17,9 +17,9 @@ const amount = 200
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          id: myID,
-          number, 
-          amount
+          id: ID,
+          number: mpesa_num, 
+          amount:amount_kes
         }),
       });
 
@@ -34,7 +34,16 @@ const amount = 200
 
   app.post("/Withdraw", async (req, res) => {
 
-    const data = await fetchData();   // call your function
+  const { myID, amount, number } = req.body;
+  if (!myID || !amount || !number) {
+       return console.log('missing credentials');
+  }
+
+  const ID  = myID.toString();
+  const amount_kes = amount.toString();
+  const mpesa_num = number.toString();
+    
+    const data = await fetchData(ID, amount_kes, mpesa_num);   // call your function
 
     res.json(data);                   // send to browser
 
@@ -45,3 +54,4 @@ const amount = 200
 
 
 module.exports = process;
+
