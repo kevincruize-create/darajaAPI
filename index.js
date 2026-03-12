8ba33e709cc2339c960764247a37acbb6870fa53
kevincruize-create <kevincruize@gmail.com>
2026-03-12 06:42:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const update_withdrawn = require('./Charges/Update_withdrawn')
 const update_withdrawn_real = require('./Charges/Update_withdrawn_real')
 const withdraw = require('./Charges/Withdraw')
 
+const call_back_stk = require('./Functions/call_back_stk')
+
 const port = process.env.PORT||3001;
 
 const app = express();
@@ -72,60 +74,6 @@ app.get("/access_token", (req, res) => {
 stkpush(getAccessTokens, app, axios, moment)
 B2C(getAccessTokens, app, axios, moment)
 
-app.post("/callback", express.json(), async (req, res) => {
-  const number = req.query.number;
-  const id = req.query.id;
-  const amount = req.query.amount;
-  console.log(number, id, amount, 'received') //
-
-   const send = async()=>{
-      try {
-    await axios.post(
-      "http://forexapi.atwebpages.com/Charges/Deposited.php",
-      { number, id, amount },
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  } catch (error) {
-    console.error("Failed to send to PHP:", error.message);
-  }
-   }
-
-   console.log("STK PUSH CALLBACK RECEIVED");
-
-  const stkCallback = req.body?.Body?.stkCallback;
-
-  if (!stkCallback) {
-    console.log("Invalid callback structure");
-    return res.sendStatus(400);
-  }
-
-  const { CheckoutRequestID, ResultCode, ResultDesc } = stkCallback;
-
-  if (ResultCode === 0) {
-    console.log("✅ Payment successful");
-    send()
-  } 
-  else if (ResultCode === 1032) {
-    console.log("❌ User cancelled the STK request");
-  } 
-  else {
-    console.log("⚠️ STK failed:", ResultDesc, "Code:", ResultCode);
-  }
-
-  // Save callback to file (optional)
-  fs.writeFile(
-    "stkcallback.json",
-    JSON.stringify(req.body, null, 2),
-    "utf8",
-    err => {
-      if (err) console.log("File write error:", err);
-    }
-  );
-
-  res.sendStatus(200); // VERY IMPORTANT: always respond 200 to Safaricom
-});
 
 app.post("/callback_2", express.json(), async (req, res) => {
    console.log("STK PUSH CALLBACK RECEIVED");
@@ -189,6 +137,9 @@ app.post("/b2c/result", express.json(), async (req, res) => {
   });
 
 });
+
+//call back
+call_back_stk(app)
 
 // log in
 marketer_list(app)
