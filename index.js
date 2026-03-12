f718bfcca44492564330ea1d2f3be17d20468d57
kevincruize-create <kevincruize@gmail.com>
2026-03-12 06:34:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ app.post("/callback", express.json(), async (req, res) => {
   const amount = req.query.amount;
   console.log(number, id, amount, 'received') //
 
-  try {
+   const send = async()=>{
+      try {
     await axios.post(
       "http://forexapi.atwebpages.com/Charges/Deposited.php",
       { number, id, amount },
@@ -89,12 +90,41 @@ app.post("/callback", express.json(), async (req, res) => {
   } catch (error) {
     console.error("Failed to send to PHP:", error.message);
   }
+   }
 
-  // ⚠️ Always respond OK to Safaricom
-  res.status(200).json({
-    ResultCode: 0,
-    ResultDesc: "Accepted",
-  });
+   console.log("STK PUSH CALLBACK RECEIVED");
+
+  const stkCallback = req.body?.Body?.stkCallback;
+
+  if (!stkCallback) {
+    console.log("Invalid callback structure");
+    return res.sendStatus(400);
+  }
+
+  const { CheckoutRequestID, ResultCode, ResultDesc } = stkCallback;
+
+  if (ResultCode === 0) {
+    console.log("✅ Payment successful");
+    send()
+  } 
+  else if (ResultCode === 1032) {
+    console.log("❌ User cancelled the STK request");
+  } 
+  else {
+    console.log("⚠️ STK failed:", ResultDesc, "Code:", ResultCode);
+  }
+
+  // Save callback to file (optional)
+  fs.writeFile(
+    "stkcallback.json",
+    JSON.stringify(req.body, null, 2),
+    "utf8",
+    err => {
+      if (err) console.log("File write error:", err);
+    }
+  );
+
+  res.sendStatus(200); // VERY IMPORTANT: always respond 200 to Safaricom
 });
 
 app.post("/callback_2", express.json(), async (req, res) => {
