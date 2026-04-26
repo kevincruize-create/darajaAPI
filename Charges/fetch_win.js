579042015103ec860c6744b396b5390257e679da
kevincruize-create <kevincruize@gmail.com>
2026-04-26 20:35:36 +0300

    Create fetch_win.js

---
+++ Charges/fetch_win.js
@@ -0,0 +1,55 @@
+const express = require('express');
+//const app = express();
+
+
+const process = (app) => {
+app.use(express.json());
+//const myID = 38
+//const user_ID = 0
+//const amount = 0
+
+  const fetchData = async (ID, amount_kes) => {
+    try {
+
+      const response = await fetch("http://forexapi.atwebpages.com/offers/collect_win.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          myID:ID,
+          amount:amount_kes
+        }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  app.post("/fetch_win", async (req, res) => {
+
+    const { myID,  amount } = req.body;
+        if (!myID || !amount ) {
+           return console.log('missing credentials');
+       }
+
+  const ID  = myID.toString();
+  const amount_kes = amount.toString();
+ 
+    
+    const data = await fetchData(ID, amount_kes);   // call your function
+
+    res.json(data);                   // send to browser
+
+  });
+
+
+}
+
+
+module.exports = process;
