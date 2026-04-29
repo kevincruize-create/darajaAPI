9ba25755f6208c445a1af59ac3f2eccece99ed96
kevincruize-create <kevincruize@gmail.com>
2026-04-29 19:21:22 +0300

    Update Confirm_offer.js

--- Offers/Confirm_offer.js
+++ Offers/Confirm_offer.js
@@ -1,1 +1,54 @@
+const express = require('express');
+//const app = express();
 
+
+const process = (app) => {
+app.use(express.json());
+//const myID = 38
+//const user_ID = 0
+//const amount = 0
+
+  const fetchData = async (ID) => {
+    try {
+
+      const response = await fetch("http://forexapi.atwebpages.com/offers/confirm_game.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          myID:ID,
+        }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  app.post("/confirm_offer", async (req, res) => {
+
+    const { myID } = req.body;
+       if (!myID ) {
+           return console.log('missing credentials');
+       }
+
+  const ID  = myID.toString();
+ 
+ 
+    
+    const data = await fetchData(ID);   // call your function
+
+    res.json(data);                   // send to browser
+
+  });
+
+
+}
+
+
+module.exports = process;
