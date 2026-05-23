b267effab4289dc69abb232b154e94d37c0412c7
kevincruize-create <kevincruize@gmail.com>
2026-05-23 22:39:31 +0300

    Update Update_room.js

--- Offers/Update_room.js
+++ Offers/Update_room.js
@@ -1,1 +1,55 @@
+const express = require('express');
+//const app = express();
+//app.use(express.json());
+
+const process = (app) => {
+app.use(express.json());
+//const name = 'Kevin Mukoya'
+//const mpesa = 254726270922
+
+//const phone = 254726270922
+
+  const fetchData = async (room_num, ID) => {
+    try {
+      const response = await fetch("http://forexapi.atwebpages.com/offers/Update_room.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          room: room_num,
+          myID:ID
+         }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  app.post("/Update_room", async (req, res) => {
+  
+   const { room, myID } = req.body;
+     if (!room || !myID) {
+      return console.log('missing credentials');
+     }
+    const room_num  = room.toString();
+    const ID  = room.toString(myID);
+    
+    const data = await fetchData(room_num, ID);   // call your function
+
+    res.json(data);                   // send to browser
+
+  });
+
+
+}
+
+
+module.exports = process;
+
 
