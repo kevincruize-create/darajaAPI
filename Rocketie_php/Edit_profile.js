bd87b2eecf7f261241bdd4433854ab6631abe9e9
kevincruize-create <kevincruize@gmail.com>
2026-06-09 16:28:48 +0300

    Update Edit_profile.js

--- Rocketie_php/Edit_profile.js
+++ Rocketie_php/Edit_profile.js
@@ -7,7 +7,7 @@ app.use(express.json());
 //const mpesa = 254726270922
 //const myID = 38
 
-  const fetchData = async (ID, names, mpesa_num, team_name) => {
+  const fetchData = async (ID, mpesa_num, team_name) => {
     try {
 
       const response = await fetch("http://forexapi.atwebpages.com/Log_in/Edit_profile.php", {
@@ -17,7 +17,6 @@ app.use(express.json());
         },
         body: JSON.stringify({
           myID: ID,
-          name: names,
           mpesa: mpesa_num,
           team: team_name,
         }),
@@ -33,23 +32,21 @@ app.use(express.json());
   };
 
   app.post("/Edit_profile", async (req, res) => {
-    const { myID, name, mpesa, team } = req.body;
-     if (!myID || !name || !mpesa || !team) {
+    const { myID, mpesa, team } = req.body;
+     if (!myID || !mpesa || !team) {
        return res.status(400).json({
          error: "Missing credentials"
        });
      }
 
-      console.log('name', name)
-      console.log('myID', myID)
-      console.log('mpesa', mpesa)
+
 
        const ID  = myID.toString();
-       const names = name.toString();
+
        const mpesa_num = mpesa.toString();
        const team_name = team.toString();
 
-    const data = await fetchData(ID, names, mpesa_num, team_name);   // call your function
+    const data = await fetchData(ID,  mpesa_num, team_name);   // call your function
 
     res.json(data);                   // send to browser
 
