737fb238fd841bd411d030fee96bc99800d84f63
kevincruize-create <kevincruize@gmail.com>
2026-06-26 17:01:20 +0300

    Update Place_offer.js

--- Rocketie/Offers/Place_offer.js
+++ Rocketie/Offers/Place_offer.js
@@ -1,1 +1,56 @@
+const express = require('express');
+//const app = express();
 
+
+const process = (app) => {
+app.use(express.json());
+//const myID = 38
+//const user_ID = 0
+//const amount = 0
+
+  const fetchData = async (ID, amount_kes, room_num) => {
+    try {
+
+      const response = await fetch("http://forexapi.atwebpages.com/Rocketie/Offers/place_offer.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          myID:ID,
+          amount:amount_kes,
+          room: room_num
+        }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  app.post("/send_offer", async (req, res) => {
+
+    const { myID,  amount } = req.body;
+        if (!myID || !amount ) {
+       return console.log('missing credentials');
+       }
+
+  const ID  = myID.toString();
+  const amount_kes = amount.toString();
+  const room_num = 300;
+    
+    const data = await fetchData(ID, amount_kes, room_num);   // call your function
+
+    res.json(data);                   // send to browser
+
+  });
+
+
+}
+
+
+module.exports = process;
