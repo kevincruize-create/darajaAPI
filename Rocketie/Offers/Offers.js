0ab519655b5a257ebae003348c52f7dbf621a060
kevincruize-create <kevincruize@gmail.com>
2026-06-28 13:38:54 +0300

    Update Offers.js

--- Rocketie/Offers/Offers.js
+++ Rocketie/Offers/Offers.js
@@ -11,7 +11,7 @@ app.use(express.json());
   const fetchData = async (ID) => {
     try {
 
-      const response = await fetch("http://forexapi.atwebpages.com/offers/display_offers.php", {
+      const response = await fetch("http://forexapi.atwebpages.com/Rocketie/Offers/display_offers.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
