1952ac6e233b9e58607f9daf07e7f46faa559198
kevincruize-create <kevincruize@gmail.com>
2026-07-05 21:07:40 +0300

    Update Rocketie_sockets.js

--- Rocketie/Sockets/Rocketie_sockets.js
+++ Rocketie/Sockets/Rocketie_sockets.js
@@ -4,6 +4,188 @@ const express = require('express');
 
 const process = (app, io) => {
 
+const Room = 300;
+ const array = [{ name: 'Kev', myID: 94, attacker:'', game_room:6, damage:'', health:40, status:'', coins:100 }];
+//const array = [];
+//const array = [];
+//console.log('hello')
+
+io.on('connection', (socket) => {
+
+  //console.log('hello')
+  console.log('User connected:', socket.id);
+
+  io.emit('array_check', array);
+
+  socket.on('push_array', (data) => {
+
+    const user = array.find(item => item.myID === data.myID && item.room === data.room);
+
+    if(!user)
+    {
+        array.push({
+         name: data.sender,
+         myID: data.myID,
+         game_room: data.room,
+         attacker:'',
+         health:100,
+         damage:'',
+         status:'',
+         coins:0
+
+        });
+    } // it pushes the array twice.
+
+   console.log('updated array', array) // it displays double entry
+
+   
+
+  })
+
+    // Receive message from client
+  socket.on('update_loss', (data) => {
+
+    // update where array contains the details of the enemy then send data to that enemy.
+
+    console.log('bomb')
+
+    const user = array.find(item => item.myID === data.myID);
+
+     if (user) {
+         
+         user.status = 'loose'
+         console.log('loss', array)
+
+     }
+
+
+    io.emit('array', array);
+
+ 
+  });
+
+  
+
+  // Receive message from client
+  socket.on('bomb_message', (data) => {
+
+    // update where array contains the details of the enemy then send data to that enemy.
+
+    console.log('bomb')
+
+    const user = array.find(item => item.ID === data.victim);
+
+     if (user) {
+        user.damage = 'bomb';
+        user.attacker = data.sender;
+        user.health = user.health - 50
+     }
+
+
+    
+
+    // emit player array
+
+    io.emit('bomb', data);
+    io.emit('array', array);
+  });
+
+
+
+    // Receive message from client
+  socket.on('freeze_controls', (data) => {
+
+    // update where array contains the details of the enemy then send data to that enemy.
+
+    const user = array.find(item => item.myID === data.victim);
+
+     if (user) {
+        user.damage = 'freeze_controls';
+        user.attacker = data.sender;
+     }
+ 
+     console.log('freeze_controls', data.victim)
+
+    io.emit('freeze_controls', data);
+
+  });
+
+      // Receive message from client
+  socket.on('update_health', (data) => {
+
+    // update where array contains the details of the enemy then send data to that enemy.
+
+    const user = array.find(item => item.myID === data.myID && item.game_room === data.room);
+
+     if (user) {
+      
+         user.health = data.health;
+         //console.log('health')
+         io.emit('array', array);
+        
+     }
+
+    //console.log(data.room, data.myID)
+
+   //  io.emit('array', array);
+
+
+   // io.emit('freeze_controls', data);
+
+  });
+
+
+      // Receive message from client
+  socket.on('take_coins', (data) => {
+
+    // update where array contains the details of the enemy then send data to that enemy.
+  console.log('coin data',data)
+  console.log('array', array)
+
+    const user = array.find(item => item.myID === data.victim);
+
+     if (user) {
+        user.damage = 'take_coins';
+        user.attacker = data.attacker;
+
+            console.log('take coins', data)//
+            io.emit('receive_coins', data);
+            io.emit('array', array);
+     }
+
+
+
+  });
+
+  socket.on('update_coins', (data) => {
+
+    // update where array contains the details of the enemy then send data to that enemy.
+
+    const user = array.find(item => item.myID === data.myID && item.game_room === data.room);
+
+     if (user) {
+        
+        
+        user.coins = data.coins + 5;
+        console.log('coins update')
+       
+     }
+
+     //console.log('coins')
+    io.emit('array', array);
+
+  });
+
+  
+
+
+
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+
+   });
+});
     
 
 
