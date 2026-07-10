69bd97b7a94c61da958c5a81fc5f6f2974e4d8d8
kevincruize-create <kevincruize@gmail.com>
2026-07-10 11:11:25 +0300

    Create Create_account.js

---
+++ Rocketie/Logins/Create_account.js
@@ -0,0 +1,57 @@
+const express = require('express');
+//const app = express();
+
+const process = (app) => {
+app.use(express.json());
+//const phone = 4556;
+//const names = 'Joe Allan Joe'
+//const whatsapp = 4455667
+
+  const fetchData = async (phone_num, names_selected) => {
+    try {
+
+      const response = await fetch("http://forexapi.atwebpages.com/Rocketie/Log_in/create_acc.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          phone: phone_num,
+          names: names_selected,
+         }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  app.post("/create_account_rocketie", async (req, res) => {
+   const { names, phone } = req.body;
+    if (!names || !phone ) {
+       return console.log('missing credentials');
+    }
+    //const phone = 4556;
+   //const names = 'Joe Allan Joe'
+  //const whatsapp = 4455667
+    const phone_num  = phone.toString();
+    const names_selected = names.toString();
+    
+
+    
+    const data = await fetchData(phone_num, names_selected);   // call your function
+
+    res.json(data);                   // send to browser
+
+  });
+
+
+}
+
+
+module.exports = process;
+
