a05eefe762370f7fcc24b27af63e7ff727a20944
kevincruize-create <kevincruize@gmail.com>
2026-08-10 00:21:54 +0300

    Create Balance_rocketie.js

---
+++ Rocketie_Mpesa/Balance_rocketie.js
@@ -0,0 +1,48 @@
+const express = require('express');
+//const app = express();
+
+const process = (app) => {
+app.use(express.json());
+//const myID = 38
+
+  const fetchData = async (ID) => {
+    try {
+
+      const response = await fetch("http://forexapi.atwebpages.com/Rocketie/Mpesa/Balance.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          myID:ID
+        }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  app.post("/Balance_rocketie", async (req, res) => {
+
+      const { myID } = req.body;
+        if (!myID) {
+          return console.log('missing credentials');
+       }
+
+    const ID  = myID.toString();
+    const data = await fetchData(ID);   // call your function
+
+    res.json(data);                   // send to browser
+
+  });
+
+
+}
+
+
+module.exports = process;
