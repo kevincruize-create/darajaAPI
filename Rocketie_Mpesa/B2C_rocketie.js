1a007d09f7a60b7669852ad37ec35b878a0b08d6
kevincruize-create <kevincruize@gmail.com>
2026-08-10 00:09:42 +0300

    Update B2C_rocketie.js

--- Rocketie_Mpesa/B2C_rocketie.js
+++ Rocketie_Mpesa/B2C_rocketie.js
@@ -98,7 +98,7 @@ app.use(express.json());
         PartyA: "4168059",
         PartyB: `${mpesa_num}`,
         Remarks: "Withdrawal",
-        QueueTimeOutURL: `https://darajaapi-2.onrender.com/b2c/result?number=${mpesa_num}&id=${ID}&amount=${amount_kes}`,
+        QueueTimeOutURL: `https://darajaapi-2.onrender.com/b2c/result_rocketie?number=${mpesa_num}&id=${ID}&amount=${amount_kes}`,
         ResultURL: `https://darajaapi-2.onrender.com/b2c/result?number=${mpesa_num}&id=${ID}&amount=${amount_kes}`,
         Occasion: "Withdrawal",
       },
