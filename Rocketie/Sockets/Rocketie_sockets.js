3507d52e8334749cabbafc538668edfa986ab749
kevincruize-create <kevincruize@gmail.com>
2026-08-13 19:32:55 +0300

    Update Rocketie_sockets.js

--- Rocketie/Sockets/Rocketie_sockets.js
+++ Rocketie/Sockets/Rocketie_sockets.js
@@ -55,7 +55,7 @@ io.on('connection', (socket) => {
          {
              io.to(data.room).emit('array', array);
 
-          // console.log('updated array', array) 
+          console.log('updated array', array) 
          }
            
     }
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
             
          }
 
-      // console.log('updated array', array) 
+      console.log('updated array', array) 
     }
 
     const playersInRoom = array.filter(item => item.room === data.room);
