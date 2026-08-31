556db1e17426344b65af862a41cafe2fb0b7fc05
kevincruize-create <kevincruize@gmail.com>
2026-08-31 12:05:51 +0300

    Create update_room_players.js

---
+++ Rocketie/Sockets/update_room_players.js
@@ -0,0 +1,40 @@
+const express = require('express');
+//const app = express();
+
+
+const process = (app, room, type) => {
+app.use(express.json());
+//const myID = 38
+//const user_ID = 0
+//const amount = 0
+
+  const fetchData = async () => {
+    try {
+
+      const response = await fetch("http://forexapi.atwebpages.com/Rocketie/Offers/Update_room_num.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          room,
+          type
+        }),
+      });
+
+      const data = await response.json();
+
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+
+}
+
+
+module.exports = process;
