2b45d86b54d8252a8839740a11903cb451a9773f
kevincruize-create <kevincruize@gmail.com>
2026-09-09 12:43:11 +0300

    Create SendRoomNotification.js

---
+++ ASV_notification/SendRoomNotification.js
@@ -0,0 +1,105 @@
+const axios = require("axios");
+
+const sendNotifications =
+  require("./SendNotification");
+
+
+async function sendRoomNotification(message) {
+
+  try {
+
+    console.log(
+      "Fetching push tokens from PHP..."
+    );
+
+    const response = await axios.get(
+      "http://forexapi.atwebpages.com/Rocketie/Log_in/Display_notf_codes.php"
+    );
+
+    console.log(
+      "PHP response:",
+      response.data
+    );
+
+    const users = response.data;
+
+    if (!Array.isArray(users)) {
+
+      throw new Error(
+        "PHP did not return an array of users"
+      );
+    }
+
+    console.log(
+      "Users received:",
+      users.length
+    );
+
+
+    const tokens = users
+      .map(
+        user => user.push_token
+      )
+      .filter(Boolean);
+
+
+    console.log(
+      "Tokens found:",
+      tokens.length
+    );
+
+
+    if (tokens.length === 0) {
+
+      throw new Error(
+        "No push tokens were found"
+      );
+    }
+
+
+    // This is the EXACT message from React
+    const notificationMessage = message;
+
+
+    console.log(
+      "Notification message:",
+      notificationMessage
+    );
+
+
+    const result =
+      await sendNotifications(
+
+        tokens,
+
+        "🚀 Rocketie",
+
+        notificationMessage,
+
+        {
+          type: "NEW_OFFER"
+        }
+
+      );
+
+
+    return result;
+
+  } catch (error) {
+
+    console.error(
+      "Failed to send room notifications:"
+    );
+
+    console.error(
+      error.response?.data ||
+      error.message
+    );
+
+    throw error;
+  }
+}
+
+
+module.exports =
+  sendRoomNotification;
