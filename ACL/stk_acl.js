61d95ca4e249b6353486ecef5e1725b851d0cf76
kevincruize-create <kevincruize@gmail.com>
2026-09-11 13:45:23 +0300

    Update stk_acl.js

--- ACL/stk_acl.js
+++ ACL/stk_acl.js
@@ -10,7 +10,7 @@ const moment = require("moment");
 const process = (getAccessToken, app, axios, moment) =>{
 app.use(express.json());
 app.post("/stkpush_acl", async (req, res) => {
-  console.log('accepted)
+  console.log('accepted')
   try {
     const { myID, amount, mpesa } = req.body;
 
